d99729dcd19475c00d85a9c2428885f4af0c0f85
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-09 18:38:10 +0530

    worked on a script to send automated message

--- automation.py
+++ automation.py
@@ -4,7 +4,14 @@ import pathlib
 from urllib.parse import quote
 import requests
 import time
+import csv
 from webbrowser import open
+
+
+
+
+
+
 from pyautogui import click, hotkey, locateOnScreen, moveTo, press, size, typewrite
 
 class InternetException(Exception):
@@ -85,3 +92,23 @@ def sendMesg(receiver: str, message: str, wait_time:int)->None:
         press("enter")
     findTextBox()
     press("enter")
+
+def parser():
+    with open("database.csv","r") as file:
+        reader = csv.reader(file)
+        iter_reader = iter(reader)
+        for( iter_reader) in iter_reader:
+            
+            print("Message sent to "+iter_reader[1])
+            sendMesg("+91"+str(iter_reader[1]),msg,wait_time=3)
+            
+
+msg = """Hello,
+Get ready as Mood Indigo is coming to delhi on 13th - 14th October with its Mini Mood Indigo 🔥
+Some of The unique incentives are: 
+1) Mood Indigo Delhi multicity certificates to all the participants and the winners of multicity 
+2) Direct Entry into the higher rounds of mood indigo in main fest in IIT Bombay. 
+3) Championship trophy - Take your college to the top of the leaderboard by winning and participating in the competitions, the college with highest points will be awarded with Delhi Multicity Champions
+Register now at my.moodi.org/multicities 
+Rule Book is attached here - https://drive.google.com/file/d/1Qsm-c9juHlEoXXmi66yweaoG7bpoi2zc/view?usp=drive_link"""
+parser()
--- main.py
+++ main.py
@@ -1,24 +1,91 @@
 import pywhatkit
 import time
+import csv
 
+import pywhatkit.sc
 # pywhatkit.sendwhatmsg('+917219201845',"Good Morning",7,14)
-index=0
-numer_dict={
-    "1":["Kamlesh Kurmi Sir","+919827736549"],
-    "2":["Mahesh Kurmi Sir ",'+918839833628'],
-    "3":["Jindal Sir","+919752611611"],
-    "4":["Prakhar Gupta","+919235527628"],
-    "5":["Aditya Gautam","+917771033480"]
-}
 
-for i in range(1,6):
-    for j in range(1,6):
-        msg=f"{numer_dict[str(i)][0]}, GoodAfter Noon {j} times"
-        try:
 
-            pywhatkit.sendwhatmsg_instantly(f"{numer_dict[str(i)][1]}",msg,tab_close=True)
-            print(f"Message sent to {numer_dict[str(i)][0]}\n")
-        except Exception:
 
-            print(f"Unable to send the message on iteration {numer_dict[str(i)][0]}")
-        time.sleep(1.5)
+def parser():
+    with open("database.csv","r") as file:
+        reader = csv.reader(file)
+        iter_reader = iter(reader)
+        for( iter_reader) in iter_reader:
+            
+            print("Message sent to "+iter_reader[1])
+            
+            pywhatkit.sendwhatmsg_instantly("+91"+str(iter_reader[1]),msg,tab_close=True,wait_time=20
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            )
+
+
+
+
+msg = """Hello,
+Get ready as Mood Indigo is coming to delhi on 13th - 14th October with its Mini Mood Indigo 🔥
+Some of The unique incentives are: 
+1) Mood Indigo Delhi multicity certificates to all the participants and the winners of multicity 
+2) Direct Entry into the higher rounds of mood indigo in main fest in IIT Bombay. 
+3) Championship trophy - Take your college to the top of the leaderboard by winning and participating in the competitions, the college with highest points will be awarded with Delhi Multicity Champions
+Register now at my.moodi.org/multicities 
+Rule Book is attached here - https://drive.google.com/file/d/1Qsm-c9juHlEoXXmi66yweaoG7bpoi2zc/view?usp=drive_link"""
+parser()
+
+# print(time.localtime())
